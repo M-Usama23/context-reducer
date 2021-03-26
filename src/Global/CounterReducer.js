52864bf276f91ec094f8d329de3936fc57c1f014
M-Usama23 <80664846+M-Usama23@users.noreply.github.com>
2021-03-26 11:40:05 +0500

    Update CounterReducer.js

--- src/Global/CounterReducer.js
+++ src/Global/CounterReducer.js
@@ -1,8 +1,11 @@
-function counterReducer(state, action){
-    switch(action){
+const counterReducer = (state, action) => {
+    switch (action) {
         case 'INCREMENT':
             return state + 1
+        default:
+            return state;
     }
 }
+
 
 export default counterReducer;
